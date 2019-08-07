ba50e74de99cd72faaafa1bb5287512bc758f5c1
Mostafa Charem <mostafa.karim.charem.moudabbes@everis.com>
2019-08-07 02:56:52 -0500

    Deleted unused exception

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
 }
