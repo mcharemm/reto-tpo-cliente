a49ab29344db61dfcfdb3a432c1cd7e25c51c914
Mostafa Charem <mostafa.karim.charem.moudabbes@everis.com>
2019-08-12 05:25:42 -0500

    added aws ecr get-login to deploy_service.sh

--- docker/deploy_service.sh
+++ docker/deploy_service.sh
@@ -4,6 +4,8 @@ docker stop service-clientes && docker rm service-clientes
 
 docker rmi 134219192027.dkr.ecr.us-east-1.amazonaws.com/reto-tpo/service-clientes
 
+eval $(aws ecr get-login --no-include-email --region us-east-1)
+
 docker run --name service-clientes \
            -d \
            --restart unless-stopped \
